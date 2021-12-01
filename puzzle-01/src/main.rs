c5c5797c83f650d6cf508c2d654f00982bad1e4a
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-01 10:07:11 +0100

    Add puzzle 01 input

--- puzzle-01/src/main.rs
+++ puzzle-01/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    let filename = "./input/input.txt";
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("File contents:\n{}", contents);
 }
