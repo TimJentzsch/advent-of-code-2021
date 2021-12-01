a9c386a5e6847aac699d61fbe9cf938e4636c3be
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-01 10:43:37 +0100

    Separate parsing from depth counting

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -7,14 +7,15 @@ fn main() {
     let filename = "./input/input.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let depth_increases = count_depth_increases(contents);
+    let depths = parse_depths(contents);
+    let depth_increases = count_depth_increases(depths);
 
     println!("The depth increased {} number of times.", depth_increases);
 }
 
-fn count_depth_increases(contents: String) -> u32 {
-    // Parse the depth values
-    let depths: Vec<u32> = contents
+/// Parse the depth values provided in the file.
+fn parse_depths(contents: String) -> Vec<u32> {
+    contents
         .split('\n')
         .into_iter()
         // Convert the line to an integer
@@ -26,8 +27,12 @@ fn count_depth_increases(contents: String) -> u32 {
         })
         // Unwrap the result
         .map(|parse_result| parse_result.unwrap())
-        .collect();
+        .collect()
+}
 
+/// Count the number of depth increases.
+fn count_depth_increases(depths: Vec<u32>) -> u32 {
+    // Parse the depth values
     let mut depth_increases = 0;
 
     for (index, depth) in depths.iter().enumerate() {
@@ -42,10 +47,20 @@ fn count_depth_increases(contents: String) -> u32 {
 }
 
 #[test]
-fn should_count_depth_increases() {
+fn should_parse_depths() {
     // Example from the task description
     let contents = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n".to_string();
-    let actual = count_depth_increases(contents);
+    let expected = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let actual = parse_depths(contents);
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn should_count_depth_increases() {
+    // Example from the task description
+    let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let actual = count_depth_increases(depths);
 
     assert_eq!(actual, 7);
 }
