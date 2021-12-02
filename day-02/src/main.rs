5fc488ac6b802a59b925012b8032108d54cda649
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-02 10:20:24 +0100

    Add day 02 input

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // Read the input file
+    let filename = "./input/input.txt";
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("{}", contents);
 }
