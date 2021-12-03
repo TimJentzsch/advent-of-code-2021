70f49dc9b0232b08ad4683dc12ffe184b1027247
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-03 14:21:26 +0100

    Add functions for oxygen generator and C02 scrubber ratings

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -29,24 +29,37 @@ fn parse_value_list(input: String) -> Vec<u16> {
         .collect()
 }
 
-/// Determine the gamma value from 
-/// 
+/// Get the bit at the given position (from the right).
+fn get_bit_at_position(value: u16, position: usize) -> u16 {
+    (value >> position) & 1
+}
+
+/// Determine the most common bit at the given position (from the right).
+fn determine_most_common_bit(values: &Vec<u16>, position: usize) -> u16 {
+    let one_count: u16 = values
+        .iter()
+        .map(|value| get_bit_at_position(*value, position))
+        .sum();
+
+    // Determine the most common bit
+    if one_count >= (values.len() as u16 - one_count) {
+        1
+    } else {
+        0
+    }
+}
+
+/// Determine the gamma value from
+///
 /// Gamma takes the most common bit and is the first value.
 /// Epsilon takes the least common bit and is the second value.
 fn determine_gamma_and_eplison(values: Vec<u16>, length: usize) -> (u16, u16) {
     let mut gamma: u16 = 0;
     let mut epsilon: u16 = 0;
 
-    let majority = values.len() as u16 / 2;
-
     for i in 0..length {
-        let one_count: u16 = values.iter().map(|value| {
-            let bit = (value >> i) & 1;
-            bit
-        }).sum();
-
         // Determine the bit values
-        let (gamma_bit, epsilon_bit): (u16, u16) = if one_count >= majority {
+        let (gamma_bit, epsilon_bit): (u16, u16) = if determine_most_common_bit(&values, i) == 1 {
             (1, 0)
         } else {
             (0, 1)
@@ -60,9 +73,54 @@ fn determine_gamma_and_eplison(values: Vec<u16>, length: usize) -> (u16, u16) {
     (gamma, epsilon)
 }
 
+/// Determine the oxygen generator rating.
+fn determine_oxygen_generator_rating(values: Vec<u16>, length: usize) -> u16 {
+    let mut values = values;
+
+    for i in (0..length).rev() {
+        let most_common_bit = determine_most_common_bit(&values, i);
+
+        values = values
+            .iter()
+            .filter(|value| {
+                let bit = get_bit_at_position(**value, i);
+                bit == most_common_bit
+            })
+            .map(|value| *value)
+            .collect();
+    }
+
+    *values.first().unwrap()
+}
+
+/// Determine the C02 scrubber rating.
+fn determine_co2_scrubber_rating(values: Vec<u16>, length: usize) -> u16 {
+    let mut values = values;
+
+    for i in (0..length).rev() {
+        let most_common_bit = determine_most_common_bit(&values, i);
+
+        values = values
+            .iter()
+            .filter(|value| {
+                let bit = get_bit_at_position(**value, i);
+                bit != most_common_bit
+            })
+            .map(|value| *value)
+            .collect();
+
+        println!("MCB: {}, Len: {}", most_common_bit, values.len());
+    }
+
+    *values.first().unwrap()
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{parse_value_list, determine_gamma_and_eplison};
+    use crate::{
+        determine_co2_scrubber_rating, determine_gamma_and_eplison, determine_most_common_bit,
+        determine_oxygen_generator_rating, parse_value_list,
+    };
 
     #[test]
     fn should_parse_value_list() {
@@ -76,10 +134,57 @@ mod test {
     }
 
     #[test]
+    fn should_determine_most_common_bit_single_value_0() {
+        let values = vec![0];
+        let expected = 0;
+        let actual = determine_most_common_bit(&values, 0);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_determine_most_common_bit_single_value_1() {
+        let values = vec![1];
+        let expected = 1;
+        let actual = determine_most_common_bit(&values, 0);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_determine_most_common_bit_multiple_values_0() {
+        let values = vec![
+            0b11110, 0b10110, 0b10111, 0b10101, 0b11100, 0b10000, 0b11001,
+        ];
+        let expected = 0;
+        let actual = determine_most_common_bit(&values, 3);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
     fn should_determine_gamma_and_epsilon() {
         let values = vec![4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10];
         let expected = (22, 9);
         let actual = determine_gamma_and_eplison(values, 5);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_determine_oxygen_generator_rating() {
+        let values = vec![4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10];
+        let expected = 23;
+        let actual = determine_oxygen_generator_rating(values, 5);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_determine_c02_scrubber_rating() {
+        let values = vec![4, 30, 22, 23, 21, 15, 7, 28, 16, 25, 2, 10];
+        let expected = 23;
+        let actual = determine_co2_scrubber_rating(values, 10);
 
         assert_eq!(actual, expected);
     }
