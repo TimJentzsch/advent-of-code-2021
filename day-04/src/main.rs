03c4f2bebc36bb235e6343037d5ec4ac1e701bb5
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-04 16:00:30 +0100

    Implement board parsing

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 struct Board<const R: usize, const C: usize> {
     /// The values of the bingo board.
     values: [[u16; R]; C],
@@ -5,10 +6,45 @@ struct Board<const R: usize, const C: usize> {
     mask: [[bool; R]; C],
 }
 
-impl<const R: usize, const C: usize> Board<{R}, {C}> {
+impl<const R: usize, const C: usize> Board<{ R }, { C }> {
     /// Create a new board with the given values.
     fn new(values: [[u16; R]; C]) -> Board<R, C> {
-        Board { values, mask: [[false; R]; C] }
+        Board {
+            values,
+            mask: [[false; R]; C],
+        }
+    }
+
+    /// Create a new board from a given board string.
+    ///
+    /// Example string:
+    ///
+    /// ```txt
+    /// 22 13 17 11  0
+    ///  8  2 23  4 24
+    /// 21  9 14 16  7
+    ///  6 10  3 18  5
+    ///  1 12 20 15 19
+    /// ```
+    fn from_input(input: String) -> Board<R, C> {
+        let mut values = [[0u16; R]; C];
+        let row_strs: Vec<&str> = input.split("\n").into_iter().collect();
+
+        // Go through all rows
+        for row in 0..R {
+            let col_str: Vec<&str> = row_strs[row].split_whitespace().into_iter().collect();
+
+            // Go through all columns
+            for col in 0..C {
+                // Parse the value at the given position
+                let val_str = col_str[col];
+                let val: u16 = val_str.parse().unwrap();
+
+                values[row][col] = val;
+            }
+        }
+
+        Board::new(values)
     }
 
     /// Mark that the given value occurred.
@@ -24,7 +60,6 @@ impl<const R: usize, const C: usize> Board<{R}, {C}> {
 
     /// Determines if this board has won, i.e. if a full row or column is marked.
     fn has_won(&self) -> bool {
-
         // Check rows
         for row in 0..R {
             let mut has_won = true;
@@ -63,4 +98,26 @@ impl<const R: usize, const C: usize> Board<{R}, {C}> {
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Board;
+
+    #[test]
+    fn should_parse_board() {
+        let input: String =
+            "22 13 17 11  0\n8  2 23  4 24\n21  9 14 16  7\n6 10  3 18  5\n1 12 20 15 19"
+                .to_string();
+        let expected: Board<5, 5> = Board::new([
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19],
+        ]);
+        let actual = Board::from_input(input);
+
+        assert_eq!(actual, expected);
+    }
 }
