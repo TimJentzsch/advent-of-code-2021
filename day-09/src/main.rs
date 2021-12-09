ab0a41b9667b0d644c1bbd55ef66af5d7ef9ae71
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-09 21:34:02 +0100

    Determine low points of the height map

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -23,6 +23,36 @@ impl<const R: usize, const C: usize> Heightmap<R, C> {
 
         Heightmap { heights }
     }
+
+    fn low_points(&self) -> Vec<u8> {
+        let mut low_points = vec![];
+
+        for row in 0..R {
+            for col in 0..C {
+                let value = self.heights[row][col];
+                let mut is_low_point = true;
+
+                // Check if the adjacent values are all higher
+                for (dr, dc) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
+                    let (r_index, c_index) = (row as i32 + dr, col as i32 + dc);
+
+                    if r_index >= 0 && r_index < R as i32 && c_index >= 0 && c_index < C as i32 {
+                        if self.heights[r_index as usize][c_index as usize] <= value {
+                            is_low_point = false;
+                            break;
+                        }
+                    }
+                    
+                }
+
+                if is_low_point {
+                    low_points.push(value);
+                }
+            }
+        }
+
+        low_points
+    }
 }
 
 fn main() {
@@ -55,6 +85,23 @@ mod tests {
             ],
         };
         let actual = Heightmap::<5, 10>::from_input(input);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_determine_low_points() {
+        let heightmap = Heightmap {
+            heights: [
+                [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+                [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+                [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+                [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+                [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+            ],
+        };
+        let expected = vec![1, 0, 5, 5];
+        let actual = heightmap.low_points();
 
         assert_eq!(actual, expected);
     }
