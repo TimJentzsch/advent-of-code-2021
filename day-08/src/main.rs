7ea4c5e3f09d9394af30c40b3ac7a8fad6314a15
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-09 01:44:21 +0100

    Find mapping from given inputs

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -49,8 +49,10 @@ impl Digit for u8 {
         let mut remapped_digit = 0u8;
 
         for index in 0..7 {
-            let bit = (self >> index) & 1;
-            remapped_digit += bit << map[index];
+            let bit = (self >> (6-index)) & 1;
+            if bit == 1 {
+                remapped_digit += map[index];
+            }
         }
 
         remapped_digit
@@ -70,7 +72,7 @@ impl Digit for u8 {
             0b1010010 => 7,
             0b1111111 => 8,
             0b1111011 => 9,
-            _ => panic!("Invalid digit value!"),
+            _ => panic!("Invalid digit value {:#07b}", self),
         }
     }
 }
@@ -131,9 +133,84 @@ fn parse_input(input: String) -> Vec<(Vec<u8>, Vec<u8>)> {
         .collect()
 }
 
+fn filter_digits_with_count(digits: &Vec<u8>, count: u8) -> Vec<u8> {
+    digits
+        .into_iter()
+        .filter(|digit| digit.segment_count() == count)
+        .map(|&digit| digit)
+        .collect()
+}
+
+fn find_digit_with_count(digits: &Vec<u8>, count: u8) -> u8 {
+    *filter_digits_with_count(digits, count).first().unwrap()
+}
+
+fn calculate_output_value(input: String) -> u64 {
+    let (input_digits, output_digits) = parse_input_line(input);
+    let mapping = determine_mapping(input_digits);
+
+    let mut value = 0;
+
+    for i in 0..4 {
+        let digit_value = output_digits[i].remap(&mapping).value() as u64;
+        println!("Value: {}", digit_value);
+        value += digit_value * 10u64.pow((3 - i).try_into().unwrap());
+    }
+
+    value
+}
+
+/// Determine the mapping of the segments.
+fn determine_mapping(digits: Vec<u8>) -> [u8; 7] {
+    // Unique numbers
+    // One
+    let cf = find_digit_with_count(&digits, 2);
+    // Four
+    let bcdf = find_digit_with_count(&digits, 4);
+    // Seven
+    let acf = find_digit_with_count(&digits, 3);
+    // Eight
+    let abcdefg = find_digit_with_count(&digits, 7);
+
+    let a = acf - cf;
+    let bd = bcdf - cf;
+
+    // Common segments of digits with five segments
+    let adg = filter_digits_with_count(&digits, 5)
+        .into_iter()
+        .reduce(|a, b| a & b)
+        .unwrap();
+
+    // Common segments of digits with six segments
+    let abfg = filter_digits_with_count(&digits, 6)
+        .into_iter()
+        .reduce(|a, b| a & b)
+        .unwrap();
+
+    let bdf = adg ^ abfg;
+    let f = bdf - bd;
+
+    let c = cf - f;
+
+    // Two
+    let acdeg = filter_digits_with_count(&digits, 5)
+        .into_iter()
+        .filter(|&digit| digit & f == 0)
+        .next()
+        .unwrap();
+    
+    let b = abcdefg - acdeg - f;
+    let d = bcdf - cf - b;
+
+    let g = adg - a - d;
+    let e = abcdefg - a - b - c - d - f - g;
+
+    [a, b, c, d, e, f, g]
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{parse_digit_list, Digit};
+    use crate::{parse_digit_list, Digit, calculate_output_value};
 
     #[test]
     fn should_count_active_segments() {
@@ -157,6 +234,27 @@ mod tests {
         let input = "eacfd acdfbe cbdegf fcbaedg".to_string();
         let expected: Vec<u8> = vec![0b1011110, 0b1111110, 0b0111111, 0b1111111];
         let actual = parse_digit_list(input);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_remap_value() {
+        let input = 0b1101010;
+        // cgafedb -> abcdefg
+        let mapping = [0b0010000, 0b0000001, 0b1000000, 0b0000010, 0b0000100, 0b0001000, 0b0100000];
+        let expected = 0b0011011;
+        
+        let actual = input.remap(&mapping);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn should_calculate_output_value() {
+        let input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf".to_string();
+        let expected = 5353u64;
+        let actual = calculate_output_value(input);
 
         assert_eq!(actual, expected);
     }
