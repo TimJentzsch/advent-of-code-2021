2829e0b8e053a604fd81122fa7f21653b5d58d2a
Tim Jentzsch <tim-jentzsch@gmx.de>
2021-12-11 17:57:47 +0100

    Add auto complete score and day 10 part 2 output

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 enum LineEvaluation {
     Ok,
     Corrupt(char),
-    Incomplete,
+    Incomplete(Vec<char>),
 }
 
 fn main() {
@@ -12,9 +12,13 @@ fn main() {
     let filename = "./input/input.txt";
     let input = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let syntax_score = syntax_error_score(input);
+    let syntax_score = syntax_error_score(input.clone());
 
     println!("Syntax error score: {}", syntax_score);
+
+    let middle_score = auto_complete_score(input);
+
+    println!("Auto complete score: {}", middle_score);
 }
 
 fn parse_line(line: String) -> LineEvaluation {
@@ -40,7 +44,7 @@ fn parse_line(line: String) -> LineEvaluation {
     }
 
     if stack.len() > 0 {
-        LineEvaluation::Incomplete
+        LineEvaluation::Incomplete(stack.into_iter().rev().collect())
     } else {
         LineEvaluation::Ok
     }
@@ -64,9 +68,45 @@ fn syntax_error_score(input: String) -> usize {
     score
 }
 
+fn auto_complete_score(input: String) -> usize {
+    let mut scores: Vec<usize> = input
+        .trim()
+        .split('\n')
+        .map(|line| parse_line(line.to_string()))
+        .filter(|eval| match eval {
+            LineEvaluation::Incomplete(_) => true,
+            _ => false,
+        })
+        .map(|eval| {
+            if let LineEvaluation::Incomplete(chars) = eval {
+                let mut score = 0;
+
+                for ch in chars {
+                    score *= 5;
+                    score += match ch {
+                        ')' => 1,
+                        ']' => 2,
+                        '}' => 3,
+                        '>' => 4,
+                        _ => 0,
+                    };
+                }
+
+                score
+            } else {
+                panic!("Unreachable!");
+            }
+        })
+        .collect();
+
+    scores.sort();
+
+    scores[scores.len() / 2]
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{LineEvaluation, parse_line, syntax_error_score};
+    use crate::{auto_complete_score, parse_line, syntax_error_score, LineEvaluation};
 
     #[test]
     fn should_parse_corrupt_line() {
@@ -88,8 +128,8 @@ mod tests {
 
     #[test]
     fn should_parse_incomplete_line() {
-        let input = "[<>({}){}[([])<>]".to_string();
-        let expected = LineEvaluation::Incomplete;
+        let input = "[({(<(())[]>[[{[]{<()<>>".to_string();
+        let expected = LineEvaluation::Incomplete(vec!['}', '}', ']', ']', ')', '}', ')', ']']);
         let actual = parse_line(input);
 
         assert_eq!(actual, expected);
@@ -106,10 +146,30 @@ mod tests {
 {<[[]]>}<{[{[{[]{()[[[]
 [<(<(<(<{}))><([]([]()
 <{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]".to_string();
+<{([{{}}[<[[[<>{}]]]>[]]"
+            .to_string();
 
         let actual = syntax_error_score(input);
 
         assert_eq!(actual, 26397);
+    }
+
+    #[test]
+    fn should_determine_auto_complete_score() {
+        let input = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]"
+            .to_string();
+
+        let actual = auto_complete_score(input);
+
+        assert_eq!(actual, 288957);
     }
 }
